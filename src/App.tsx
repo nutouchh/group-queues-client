d6e3872ac810130405fbae99d267d240724e62eb
Nike <nkaz2003@gmail.com>
2023-03-03 21:21:11 +0300

    nk/dev initial configuration

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+import './App.scss';
+import { MainPage } from './pages/MainPage/MainPage';
+
+const App: FC = () => {
+	return (
+		<div className='App'>
+			<MainPage></MainPage>
+		</div>
+	);
+};
+
+export default App;
